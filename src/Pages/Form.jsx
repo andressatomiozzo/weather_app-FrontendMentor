8f890cedf1bec3d14fb0b3b181bc969a703510fd
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-06-10 17:41:17 -0400

    estilizei o formulário

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import styles from "./Form.module.css"
 import useForm from "../Hooks/useForm";
 import SearchContext from "../createContext/SearchContext";
 
+
 const Form = () => {
-  const { value, error, onChange, onBlur } = useForm();
+  const { value, onChange, onBlur } = useForm();
   const { searchCity } = React.useContext(SearchContext);
 
   const handleSubmit = (event) => {
@@ -12,22 +14,23 @@ const Form = () => {
   };
 
   return (
-    <div>
-      <h1>How's the sky looking today?</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="search"
-          name="search"
-          placeholder="Search for a place..."
-          value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-        />
-        {error && <span>{error}</span>}
-        <button>Search</button>
+    <>
+      <h1 className={styles.title}>How's the sky looking today?</h1>
+      <form onSubmit={handleSubmit} className={styles.form}>
+          <input
+            type="text"
+            id="search"
+            name="search"
+            placeholder='Search for a place...'
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+            className={styles.input}
+          />
+
+        <button className={styles.button}>Search</button>
       </form>
-    </div>
+    </>
   );
 };
 
